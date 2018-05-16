a37bb70522e8ff3278cf9fd9414ebb437dc80ddd
Stephen Newport <snewport@korio.ca>
2018-05-15 23:00:11 -0400

    Added CORS configuration.

--- src/main/kotlin/ai/korio/services/KorioServices.kt
+++ src/main/kotlin/ai/korio/services/KorioServices.kt
@@ -6,12 +6,35 @@ import org.camunda.bpm.engine.ProcessEngine
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @SpringBootApplication
 @EnableProcessApplication
+@Configuration
 class KorioServicesApplication {
 
     val processEngine: ProcessEngine by CamundaEngineConfig()  // calling a global instance of the process engine
+
+    @Bean
+    fun processCorsFilter(): FilterRegistrationBean<*> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("*")
+        source.registerCorsConfiguration("/**", config)
+
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.setOrder(0)
+        return bean
+    }
 }
 
 fun main(args: Array<String>) {
